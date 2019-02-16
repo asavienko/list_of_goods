b1dbe9a0c85f1142a5abfd5f69dc1874d17a8330
Anton <asavienko@gmail.com>
2019-02-16 22:17:58 +0200

    modified .gitignore

--- src/ProductListComponent.js
+++ src/ProductListComponent.js
@@ -7,26 +7,24 @@ class ProductListComponent extends React.Component {
 
   render() {
     const rows = [];
-    let lastCategory = null
+    let lastCategory = null;
 
     this.props.products.forEach(({category, name, price, stocked}) => {
-        if (category !== lastCategory ) {
-          rows.push(<ProductCategoryRow
-            category={category}
-            key={category}
+      if (category !== lastCategory) {
+        rows.push(<ProductCategoryRow
+          category={category}
+          key={category}
 
-          />);
+        />);
 
-          lastCategory = category;
-        }
+        lastCategory = category;
+      }
       rows.push(<ProductRow
         name={name}
         price={price}
         stocked={stocked}
       />);
-      }
-    )
-    ;
+    });
 
     return <table>
       <thead>
